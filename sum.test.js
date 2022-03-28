fe40f29130e5519b4efeea3cb4c5c0cf1210b16d
Liam Carstens <93952888+LordInfernus09@users.noreply.github.com>
2022-03-28 19:03:16 +0200

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -3,8 +3,3 @@ const sum = require("./sum.js");
 test("adds 2 + 3 to equal 5" , () => {
   expect(sum(2,3)).toBe(5);
 })
-
-const multiply = require("./sum.js");
-test("multiply 2 and 3 to equal 6" , () => {
-  expect(multiply(2,3)).toBe(6);
-})
