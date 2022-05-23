6bdc167b41d531f38963eb1894677b61a8f863eb
Liam Carstens <93952888+LordInfernus09@users.noreply.github.com>
2022-05-23 10:13:48 +0200

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -4,7 +4,4 @@ test("adds 2 + 3 to equal 5" , () => {//test first function
   expect(sum(2,3)).toBe(5);
 });
 
-test("2 * 3 to equal 6" , () => {//test second function
-  expect(multiply(2,3)).toBe(6);
-});
 
