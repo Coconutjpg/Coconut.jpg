90aef9c884302cc04fc460594408f31c13287c15
Liam Carstens <93952888+LordInfernus09@users.noreply.github.com>
2022-05-23 10:15:18 +0200

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-const {sum, multiply} = require("./sum.js");//import multiple functions from same file
+const {sum, multiply, divide} = require("./sum.js");//import multiple functions from same file
 
 test("adds 2 + 3 to equal 5" , () => {//test first function
   expect(sum(2,3)).toBe(5);
